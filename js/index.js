c16c88c350105a3c3b5fd0aef4ada8ac0554e675
Gabriel Ojeda <gojedaor@Gabriels-MacBook-Pro.local>
2022-04-21 13:33:23 -0400

    se separo el evento en click y enter

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 const input = document.querySelector('#texto')
+const button = document.querySelector('#btnSearch')
 const formSearch = document.querySelector('#formSearch')
 
-formSearch.addEventListener('submit', (e) => {
+button.addEventListener('click', (e) => {
 	e.preventDefault()
 	search()
+	ga('send', 'event', 'homevariante', 'click-buscador', texto)
+})
+input.addEventListener('keyup', (e) => {
+
+	if(e.keycode === '13'){
+		e.preventDefault()
+	search()
+	ga('send', 'event', 'searchTest', 'enter-buscador', texto)
+	}
 })
 
 const search = () => {
 	let texto = input.value.toLowerCase().trim()
 
 	sessionStorage.setItem('searchTest', texto)
-	ga('send', 'event', 'searchTest', 'search', texto)
+	
 
 	setTimeout(() => {
 		window.location.href = './result.html'
